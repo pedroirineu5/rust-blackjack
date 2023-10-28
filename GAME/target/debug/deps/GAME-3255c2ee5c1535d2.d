582ae8eaadc86d77ec1d522ebf2a9a6ec2825d28
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2023-10-28 12:50:30 -0300

    iniciando essa porra

---
+++ GAME/target/debug/deps/GAME-3255c2ee5c1535d2.d
@@ -0,0 +1,5 @@
+C:\ESTUDOS\rust-janderson\GAME\target\debug\deps\GAME-3255c2ee5c1535d2.rmeta: src\main.rs
+
+C:\ESTUDOS\rust-janderson\GAME\target\debug\deps\GAME-3255c2ee5c1535d2.d: src\main.rs
+
+src\main.rs:
