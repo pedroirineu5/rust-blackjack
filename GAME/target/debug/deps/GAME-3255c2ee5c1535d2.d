8e0cebfc807bf204f1aad75a19066b8b70f0829e
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2023-10-28 13:28:34 -0300

    hjgfh

--- GAME/target/debug/deps/GAME-3255c2ee5c1535d2.d
+++
@@ -1,5 +0,0 @@
-C:\ESTUDOS\rust-janderson\GAME\target\debug\deps\GAME-3255c2ee5c1535d2.rmeta: src\main.rs
-
-C:\ESTUDOS\rust-janderson\GAME\target\debug\deps\GAME-3255c2ee5c1535d2.d: src\main.rs
-
-src\main.rs:
--- GAME/target/debug/deps/GAME-fb2bb345b28b874f.d
+++
@@ -1,5 +0,0 @@
-C:\ESTUDOS\rust-janderson\GAME\target\debug\deps\GAME-fb2bb345b28b874f.rmeta: src\main.rs
-
-C:\ESTUDOS\rust-janderson\GAME\target\debug\deps\GAME-fb2bb345b28b874f.d: src\main.rs
-
-src\main.rs:
